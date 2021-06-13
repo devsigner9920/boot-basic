8d98909ff02074d02329ba8c9b9a7262aa8fc443
devsigner <devsigner9920@icloud.com>
2021-06-13 22:37:47 +0900

    first commit

---
+++ src/main/java/me/devsign/devsignerhome/DevsignerHomeApplication.java
@@ -0,0 +1,13 @@
+package me.devsign.devsignerhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsignerHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsignerHomeApplication.class, args);
+	}
+
+}
