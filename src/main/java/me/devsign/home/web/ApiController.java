8045f8d1ef6d3a34b2ed41a4932dc765de19dc92
devsigner <devsigner9920@icloud.com>
2021-06-18 23:43:53 +0900

    commit

--- src/main/java/me/devsign/home/web/ApiController.java
+++ src/main/java/me/devsign/home/web/ApiController.java
@@ -1,5 +1,12 @@
 package me.devsign.home.web;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ApiController {
-    
+    @PostMapping(path = "/api/{token}/test/select")
+    public void tryController() {
+        
+    }
 }
