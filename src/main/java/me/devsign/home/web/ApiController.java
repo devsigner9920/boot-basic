f0bb26c1bcf7bd9b9dcc67bfd9fec96df07b8332
devsigner <devsigner9920@icloud.com>
2021-06-20 21:35:34 +0900

    commit

--- src/main/java/me/devsign/home/web/ApiController.java
+++ src/main/java/me/devsign/home/web/ApiController.java
@@ -1,12 +1,14 @@
 package me.devsign.home.web;
 
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
-    @PostMapping(path = "/api/{token}/test/select")
-    public void tryController() {
-        
+    @PostMapping(path = "/api/test/return/object")
+    public Optional<? extends Object> returnObject() {
+        return Optional.empty();
     }
 }
