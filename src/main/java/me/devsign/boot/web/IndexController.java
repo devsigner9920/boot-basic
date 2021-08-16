7b008dfbdb44ec80666f2ffa9dd18626768cac49
devsigner <devsigner9920@icloud.com>
2021-08-16 23:46:28 +0900

    commit spring boot learn

--- src/main/java/me/devsign/boot/web/IndexController.java
+++ src/main/java/me/devsign/boot/web/IndexController.java
@@ -1,6 +1,7 @@
 package me.devsign.boot.web;
 
 import lombok.RequiredArgsConstructor;
+import me.devsign.boot.config.auth.dto.SessionUser;
 import me.devsign.boot.service.posts.PostsService;
 import me.devsign.boot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
@@ -8,14 +9,31 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        /*
+         * (SessionUser) httpSession.getAttribute("user")
+         * 앞서 작성된 CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성했다.
+         * 즉, 로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져온다.
+         */
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        /*
+         * if(user != null)
+         * 세션에 저장된 값이 있을 때만 model에 userName으로 등록한다.
+         * 세션에 저장된 값이 없으면 model엔 아무런 값이 없는 상태이니 로그인 버튼이 보이게 된다.
+         */
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
